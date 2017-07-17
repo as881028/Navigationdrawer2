152e32c400c3318d6f8a4b8a2faf79817b6f46ee
Jack <a0989892835@gmail.com>
2017-07-17 18:45:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/navigationdrawer/latest_news.java
@@ -0,0 +1,15 @@
+package com.example.user.navigationdrawer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by USER on 2017/7/17.
+ */
+
+public class latest_news extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.latest_news_main);
+    }
+}
